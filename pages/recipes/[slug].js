f4bef5c5e23df6c254e321782f34268a2c370a28
syedshaon <syedshaon99@gmail.com>
2024-07-16 09:55:00 +0600

    icr

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import Image from "next/image";
+import Skeleton from "@/components/Skeleton";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -37,8 +38,9 @@ export const getStaticProps = async ({ params }) => {
 };
 
 export default function RecipeDetails({ recipe }) {
+  if (!recipe) return <Skeleton />;
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields;
-  console.log(method);
+  // console.log(method);
 
   return (
     <div>
